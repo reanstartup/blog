71738b40859c502eb26c8c5ff1330f6965e05646
reanstartup <reanstartup@gmail.com>
2024-10-30 15:34:16 -0700

    edit favicon

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import sitemaps
 from django.contrib.sitemaps.views import sitemap
+from django.views.static import serve
+from django.views.generic import RedirectView
 from . import views
 from blog.sitemaps import BlogPostSitemap, StaticViewSitemap
 
@@ -17,7 +19,8 @@ urlpatterns = [
     path('about/', views.about_us, name='about_us'),
     path('subscribe/', views.subscribe_mailing_list, name='subscribe_mailing_list'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-]
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon/favicon.ico')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myblog/settings.py
+++ myblog/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static'),  # Add your app's static directory
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -166,5 +166,3 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
 }
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
