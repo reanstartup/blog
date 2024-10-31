4fbac8d52c689156ccca573d2d1af61478f0bf40
reanstartup <reanstartup@gmail.com>
2024-10-30 17:55:17 -0700

    edit settings 2

--- myblog/settings.py
+++ myblog/settings.py
@@ -34,9 +34,9 @@ firebase_admin.initialize_app(cred)
 SECRET_KEY = 'django-insecure-0ybsk7h-nn&-mx0sda(0y*+a6s^up4jd5n&a-1b9bi_*o6=n$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = 'FALSE'
 
-ALLOWED_HOSTS = ['reanstartup.pythonanywhere.com','www.reanstartup.com','127.0.0.1','reanstartup.com']
+ALLOWED_HOSTS = ['www.reanstartup.com','reanstartup.com','reanstartup.pythonanywhere.com','127.0.0.1',]
 
 # Application definition
 
